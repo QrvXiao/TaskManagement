7a8b41187421151fb19e993d6117d502db22f257
Troy Xiao <t4xiao@uwaterloo.ca>
2025-09-26 15:54:25 -0400

    prerender fix

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,15 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  // 静态路由使用预渲染
+  { path: '', renderMode: RenderMode.Prerender },
+  { path: 'login', renderMode: RenderMode.Prerender },
+  { path: 'new', renderMode: RenderMode.Prerender },
+  
+  // 动态路由使用服务器端渲染
+  { path: 'task/:id', renderMode: RenderMode.Server },
+  { path: 'edit/:id', renderMode: RenderMode.Server },
+  
+  // 其他路由默认使用服务器端渲染
+  { path: '**', renderMode: RenderMode.Server }
 ];
