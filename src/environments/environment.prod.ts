9bcce2ed0125e5b8ec9a008f06312a590d26976f
Troy Xiao <t4xiao@uwaterloo.ca>
2025-09-26 15:26:10 -0400

    used real api url in environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://task-management-api-xxxx.onrender.com'  // 替换为你的 Render 后端 URL
+  apiUrl: 'https://taskmanagement-zzum.onrender.com'
 };
